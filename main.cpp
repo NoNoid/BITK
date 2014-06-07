b2bdd194851172dc3b8c1fed6df5e94b240b8eb8
NoNoid <NoNoid@gmx-topmail.de>
2014-06-07 14:29:50 +0200

    a commit

--- main.cpp
+++ main.cpp
@@ -11,10 +11,57 @@
 
 using namespace cv;
 
+Rect crateSearchFrameFromRegionOfInterest(Size videoDimensions, Rect regionOfInterest)
+{
+    Rect searchFrame(
+                regionOfInterest.x-(regionOfInterest.width/2),
+                regionOfInterest.y-(regionOfInterest.height/2),
+                2*regionOfInterest.width,
+                2*regionOfInterest.height);
+    if(searchFrame.x <= 0)
+        searchFrame.x = 0;
+
+    if(searchFrame.x >= videoDimensions.width-searchFrame.width)
+        searchFrame.x = videoDimensions.width-searchFrame.width;
+
+    if(searchFrame.y <= 0)
+        searchFrame.y = 0;
+
+    if(searchFrame.y >= videoDimensions.height-searchFrame.height)
+        searchFrame.y = videoDimensions.height-searchFrame.height;
+
+    return searchFrame;
+}
+
+void drawRectangle(const Rect &rectangleToDraw,Mat &matrixToDrawTheRectangleIn,const Scalar &color)
+{
+    line(matrixToDrawTheRectangleIn,
+         Point(rectangleToDraw.x,rectangleToDraw.y),
+         Point(rectangleToDraw.x+rectangleToDraw.width,rectangleToDraw.y),
+         color,
+         2,8);
+    line(matrixToDrawTheRectangleIn,
+         Point(rectangleToDraw.x+rectangleToDraw.width,rectangleToDraw.y),
+         Point(rectangleToDraw.x+rectangleToDraw.width,rectangleToDraw.y+rectangleToDraw.height),
+         color,
+         2,8);
+    line(matrixToDrawTheRectangleIn,
+         Point(rectangleToDraw.x+rectangleToDraw.width,rectangleToDraw.y+rectangleToDraw.height),
+         Point(rectangleToDraw.x,rectangleToDraw.y+rectangleToDraw.height),
+         color,
+         2,8);
+    line(matrixToDrawTheRectangleIn,
+         Point(rectangleToDraw.x,rectangleToDraw.y+rectangleToDraw.height),
+         Point(rectangleToDraw.x,rectangleToDraw.y),
+         color,
+         2,8);
+}
+
 int main(int, char**)
 {
     //diable printf() buffering
     setbuf(stdout, NULL);
+    printf("press 'c' to close");
 
     char cCurrentPath[FILENAME_MAX];
 
@@ -47,10 +94,14 @@ int main(int, char**)
     namedWindow(matchingWindowName,CV_WINDOW_AUTOSIZE);
 
     // define location of sub matrices in image
-    Rect regionOfInterest( 100, 100, 100, 100 );
+    Rect regionOfInterest( videoDimensions.width/2, videoDimensions.height/2, 50, 50 );
+    Rect searchFrame = crateSearchFrameFromRegionOfInterest(videoDimensions, regionOfInterest);
+
     cv::Point *bestMatchPositionsByFrame = new cv::Point[numberOfVideoFrames];
 
 //    Mat edges;
+    double oldMinVal, oldMaxVal;
+    Point oldMinLoc, oldMaxLoc, oldMatchLoc;
 
     for(unsigned int i = 0; i < numberOfVideoFrames; ++i)
     {
@@ -61,17 +112,19 @@ int main(int, char**)
 //        cvtColor(frame, edges, CV_BGR2GRAY);
 //        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
 //        Canny(edges, edges, 0, 30, 3);
+        searchFrame = crateSearchFrameFromRegionOfInterest(videoDimensions, regionOfInterest);
+        Mat searchRegion(frame,searchFrame);
 
         // define sub matrices in main matrix
         Mat RegionOfInterestMatrix(frame,regionOfInterest);
 
-        int result_cols =  frame.cols - RegionOfInterestMatrix.cols + 1;
-        int result_rows = frame.rows - RegionOfInterestMatrix.rows + 1;
+        int result_cols =  searchRegion.cols - RegionOfInterestMatrix.cols + 1;
+        int result_rows = searchRegion.rows - RegionOfInterestMatrix.rows + 1;
 
         Mat result(result_cols,result_rows,CV_32FC1);
 
         /// Do the Matching and Normalize
-        matchTemplate( frame, RegionOfInterestMatrix, result, CV_TM_CCOEFF_NORMED );
+        matchTemplate( searchRegion, RegionOfInterestMatrix, result, CV_TM_CCOEFF_NORMED );
         normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat());
 
         double minVal, maxVal;
@@ -80,6 +133,7 @@ int main(int, char**)
         minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
 
         matchLoc = minLoc;
+        oldMatchLoc = matchLoc;
         bestMatchPositionsByFrame[i] = matchLoc;
         bestMatchPositionsByFrame[i].x += regionOfInterest.width/2;
         bestMatchPositionsByFrame[i].y += regionOfInterest.height/2;
@@ -111,13 +165,16 @@ int main(int, char**)
 //        if(i > 1)
 //        line(drawFrame,bestMatchPositionsByFrame[i-1],bestMatchPositionsByFrame[i],Scalar( 255, 255, 255 ),2,8);
 
-        printf("best Match Position: %d , %d\n",matchLoc.x,matchLoc.y);
+        //printf("best Match Position: %d , %d\n",matchLoc.x,matchLoc.y);
+
+        drawRectangle(searchFrame,drawFrame,Scalar(0,255,0));
+        drawRectangle(regionOfInterest,drawFrame,Scalar(255,0,0));
 
         imshow(drawFrameWindowName, drawFrame);
         imshow(ROIWindowName, RegionOfInterestMatrix);
         imshow(matchingWindowName, result );
 
-        if(waitKey(30) >= 0) break;
+        if(waitKey(30) == 'c') break;
     }
 
     delete[] bestMatchPositionsByFrame;
